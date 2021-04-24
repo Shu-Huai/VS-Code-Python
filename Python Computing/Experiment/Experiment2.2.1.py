0c484b6a5eade107b2ac5e516dde9e10f0fd18f5
殊怀丶 <lvzhihe_123@qq.com>
2021-04-24 21:40:56 +0800

    formatted code

--- Python Computing/Experiment/Experiment2.2.1.py
+++ Python Computing/Experiment/Experiment2.2.1.py
@@ -3,7 +3,7 @@ from math import sqrt
 from math import factorial
 
 sum = 0
-for k in range(30):
+for k in range(3):
     sum += factorial(4 * k) * (1103 + 26390 * k) / factorial(k)**4 / 396**(4 * k)
 calculatedPi = 1 / (sum * 2 * sqrt(2) / 9801)
-print("The pi in math is: %.20f.\nThe pi in formula is: %.20f." % (pi, calculatedPi))
+print("The pi in math is: %.15f.\nThe pi in formula is: %.15f." % (pi, calculatedPi))
